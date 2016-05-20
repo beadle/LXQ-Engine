6b1dda0230d6da70bd9afcea86ad9c2e36a2124c
unknown <xxn2781@GIH-D-9342.game.ntes>
2016-05-20 20:40:37 +0800

    初始化项目

---
+++ LXQ/Main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+#include "Math/Math.h"
+#include "SimpleMath.h"
+
+using namespace LXQ;
+
+int WINAPI
+WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
+{
+
+	return 0;
+}
